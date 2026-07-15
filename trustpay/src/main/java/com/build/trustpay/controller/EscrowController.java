bddd29636d9ea76adf3946ad48eb3565ab653515
Ishika Thakur <ishikathakur0708@gmail.com>
2026-07-15 18:22:10 +0530

    escrow controller

--- trustpay/src/main/java/com/build/trustpay/controller/EscrowController.java
+++ trustpay/src/main/java/com/build/trustpay/controller/EscrowController.java
@@ -1,45 +1,35 @@
-package com.build.trustpay.controller;
+package com.trustpay.controller;
 
-import com.build.trustpay.dto.CreateEscrowRequest;
-import com.build.trustpay.model.Escrow;
-import com.build.trustpay.service.EscrowService;
-import org.springframework.http.ResponseEntity;
+import com.trustpay.dto.CreateEscrowRequest;
+import com.trustpay.model.Escrow;
+import com.trustpay.service.EscrowService;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
+
 @RestController
 @RequestMapping("/api/escrows")
+@CrossOrigin(origins = "http://localhost:5173")
 public class EscrowController {
-
 
     private final EscrowService escrowService;
 
-    // Spring automatically injects the EscrowService here
     public EscrowController(EscrowService escrowService) {
         this.escrowService = escrowService;
-        System.out.println(">>> EscrowController CREATED <<<");
     }
 
-    // POST http://localhost:8080/api/escrows
     @PostMapping
-    public ResponseEntity<Escrow> createEscrow(@RequestBody CreateEscrowRequest request) {
-        Escrow created = escrowService.createEscrow(request);
-        return ResponseEntity.ok(created);
+    public Escrow createEscrow(@RequestBody CreateEscrowRequest request) {
+        return escrowService.createEscrow(request);
     }
 
-    // GET http://localhost:8080/api/escrows
     @GetMapping
-    public ResponseEntity<List<Escrow>> getAllEscrows() {
-        return ResponseEntity.ok(escrowService.getAllEscrows());
+    public List<Escrow> getAllEscrows() {
+        return escrowService.getAllEscrows();
     }
 
-    // GET http://localhost:8080/api/escrows/{id}
     @GetMapping("/{id}")
-    public ResponseEntity<Escrow> getEscrowById(@PathVariable String id) {
-        Escrow escrow = escrowService.getEscrowById(id);
-        if (escrow == null) {
-            return ResponseEntity.notFound().build();
-        }
-        return ResponseEntity.ok(escrow);
+    public Escrow getEscrowById(@PathVariable Long id) {
+        return escrowService.getEscrowById(id);
     }
 }
